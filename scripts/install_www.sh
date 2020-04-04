2daf96a4ca42faef7b90c5a177bd43e615258e6c
richinseattle <richinseattle@gmail.com>
2020-04-04 07:38:56 +0000

    mofix install_www.sh

--- scripts/install_www.sh
+++ scripts/install_www.sh
@@ -7,23 +7,19 @@ FUZZFLOW_DIR=$(dirname "${SCRIPT_DIR}")
 
 sudo apt-get install -y build-essential python-dev python-pip nginx nginx-extras uwsgi uwsgi-plugin-python
 
-sed -i "s#FUZZFLOW_WWW#${FUZZFLOW_DIR}/www/frontend#g" nginx.conf
-sudo cp fuzzflow_nginx.conf /etc/nginx/sites-available/fuzzflow
-sudo ln -s /etc/nginx/sites-available/fuzzflow  /etc/nginx/sites-enabled/fuzzflow
+cd "$SCRIPT_DIR"
+sed -e "s#FUZZFLOW_WWW#${FUZZFLOW_DIR}/www/frontend#g" fuzzflow_nginx.conf | sudo tee /etc/nginx/sites-available/fuzzflow
+sudo ln -sf /etc/nginx/sites-available/fuzzflow  /etc/nginx/sites-enabled/fuzzflow
 
-touch /tmp/fuzzflow.sock
+sudo touch /tmp/fuzzflow.sock
 sudo chown www-data:www-data /tmp/fuzzflow.sock
 
-sudo cat >>/etc/uwsgi/apps-available/fuzzflow.ini<<EOL
-vhost = true
-socket = /tmp/fuzzflow.sock
-venv = "$FUZZFLOW_DIR/../.env"
-chdir = "$FUZZFLOW_DIR/../"
-module = server
-callable = app
-EOL
+sed -e "s#FUZZFLOW_DIR#${FUZZFLOW_DIR}#g" fuzzflow_uwsgi.ini | sudo tee /etc/uwsgi/apps-available/fuzzflow.ini
 
-sudo ln -s /etc/uwsgi/apps-available/fuzzflow.ini /etc/uwsgi/apps-enabled/fuzzflow.ini
+sudo ln -sf /etc/uwsgi/apps-available/fuzzflow.ini /etc/uwsgi/apps-enabled/fuzzflow.ini
+
+sudo chown -R www-data "$FUZZFLOW_DIR/www"
+sudo chown www-data "$FUZZFLOW_DIR/fuzzsvc/app/moflow.db"
 
 sudo service nginx restart
 sudo service uwsgi restart
