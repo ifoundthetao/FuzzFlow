3ceb3f4cc2a0cddf38ce68199ae34a9acf1f8ec4
Timothy Bolton <bolton.timothy@gmail.com>
2020-06-05 01:17:19 -0500

    Fix venv to copy instead of symlink.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,9 @@ SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 FUZZFLOW_DIR=$(dirname "$SCRIPT_DIR")
 
-sudo apt-get install -y build-essential python3-dev python3-pip
+sudo apt-get install -y build-essential python3-dev python3-pip python3-venv
 
-virtualenv "${FUZZFLOW_DIR}/.env" --always-copy
+env VIRTUALENV_ALWAYS_COPY=1 python3 -m venv "${FUZZFLOW_DIR}/.env"
 source "${FUZZFLOW_DIR}/.env/bin/activate"
-pip install -r "${FUZZFLOW_DIR}/scripts/requirements.txt"
+pip3 install -r "${FUZZFLOW_DIR}/scripts/requirements.txt"
 deactivate
