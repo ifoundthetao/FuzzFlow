de555adc11f9d2e047d911e10bca600916f451aa
Timothy Bolton <bolton.timothy@gmail.com>
2020-06-05 01:04:57 -0500

    remove virtualenv, as venv is in the python3 standard library.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,7 @@ SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 FUZZFLOW_DIR=$(dirname "$SCRIPT_DIR")
 
-sudo apt-get install -y build-essential python-dev python-pip
-sudo pip install virtualenv
+sudo apt-get install -y build-essential python3-dev python3-pip
 
 virtualenv "${FUZZFLOW_DIR}/.env" --always-copy
 source "${FUZZFLOW_DIR}/.env/bin/activate"
